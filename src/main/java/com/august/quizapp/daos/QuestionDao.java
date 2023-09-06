20f8daa16f4b2357e4aa67a4d615bf38cc3004db
KigwanaAugustine <augustinekigz45@gmail.com>
2023-09-06 10:56:16 +0300

    All CRUD with response entity

--- src/main/java/com/august/quizapp/daos/QuestionDao.java
+++ src/main/java/com/august/quizapp/daos/QuestionDao.java
@@ -3,12 +3,13 @@ package com.august.quizapp.daos;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-// import org.springframework.stereotype.Repository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.august.quizapp.models.Question;
 
-@RepositoryRestResource(collectionResourceRel = "questions", path = "questions")
+// @RepositoryRestResource(collectionResourceRel = "questions", path = "questions")
+@Repository
 public interface QuestionDao extends JpaRepository<Question, Integer>{
 
     List<Question> findAllByCategory(String categoryName);
